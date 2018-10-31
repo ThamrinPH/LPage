3acad62dba92841e0df1adfb429144abda37ce37
ThamrinPH <husni.sosnet@gmail.com>
2018-10-31 09:29:28 +0700

    Initial Settings

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
+
+Route::get('/test','TestController@test_index');
